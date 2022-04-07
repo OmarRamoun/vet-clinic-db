a6430c3a3c44986ec65bf866cee2d5869fe507b9
MrRamoun <omartarekramoun@gmail.com>
2022-04-07 12:51:52 +0200

    :heavy_check_mark: Create table of animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,21 @@
 /* Database schema to keep the structure of entire database. */
 
+/*
+POSTGRESQL
+Create a table named animals with the following columns:
+    id: integer
+    name: string
+    date_of_birth: date
+    escape_attempts: integer
+    neutered: boolean
+    weight_kg: decimal
+*/
+
 CREATE TABLE animals (
-    name varchar(100)
+    id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY  NOT NULL,
+    name            VARCHAR(255)                         NOT NULL,
+    date_of_birth   DATE                                 NOT NULL,
+    escape_attempts INT                                  NOT NULL,
+    neutered        BOOLEAN                              NOT NULL,
+    weight_kg       DECIMAL                              NOT NULL
 );
