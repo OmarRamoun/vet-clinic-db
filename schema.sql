f595c0e8356f6ad755e619eedb586c671394b5f5
MrRamoun <omartarekramoun@gmail.com>
2022-04-08 03:14:43 +0200

    :x: Remove commented code

--- schema.sql
+++ schema.sql
@@ -1,16 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-/*
-POSTGRESQL
-Create a table named animals with the following columns:
-    id: integer
-    name: string
-    date_of_birth: date
-    escape_attempts: integer
-    neutered: boolean
-    weight_kg: decimal
-*/
-
 CREATE TABLE animals (
     id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY  NOT NULL,
     name            VARCHAR(255)                         NOT NULL,
